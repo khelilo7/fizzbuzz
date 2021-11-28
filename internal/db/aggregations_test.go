f85f4963c1ec534ec0f3751a6b0479bb09e2142a
khalil <jridikhalil1995@gmail.com>
2021-11-28 20:44:12 +0100

    feat(test): complete test

--- internal/db/aggregations_test.go
+++ internal/db/aggregations_test.go
@@ -1,3 +1,27 @@
 package db_test
 
-// TODO
+import (
+	"testing"
+
+	"github.com/maxatome/go-testdeep/td"
+
+	"lbc/fizzbuzz/internal/db"
+)
+
+func TestAggReqCount(t *testing.T) {
+	req, err := db.AggReqCount(db.NewDBConn())
+	td.CmpNotNil(t, req)
+	td.CmpNoError(t, err)
+}
+
+func TestAggFieldCount(t *testing.T) {
+	// error
+	req, err := db.AggFieldCount(db.NewDBConn(), "")
+	td.CmpNil(t, req)
+	td.CmpError(t, err)
+
+	// no error
+	req, err = db.AggFieldCount(db.NewDBConn(), "int1")
+	td.CmpNotNil(t, req)
+	td.CmpNoError(t, err)
+}
--- internal/db/stats_test.go
+++ internal/db/stats_test.go
@@ -1,3 +1,15 @@
 package db_test
 
-// TODO
+import (
+	"testing"
+
+	"github.com/maxatome/go-testdeep/td"
+
+	"lbc/fizzbuzz/internal/db"
+)
+
+func TestGetStats(t *testing.T) {
+	data, err := db.GetStats(db.NewDBConn())
+	td.CmpNotNil(t, data)
+	td.CmpNoError(t, err)
+}
